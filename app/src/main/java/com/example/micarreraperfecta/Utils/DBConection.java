5b787e0e6a26dae7b00a4b9bed5876c0f865572e
Veroarenas96 <veronicarenas96@gmail.com>
2023-05-29 20:56:35 -0300

    Commit inicial +Clase de conección a la BD +Dependencias necesarias para ejecución

---
+++ app/src/main/java/com/example/micarreraperfecta/Utils/DBConection.java
@@ -0,0 +1,58 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class DBConection {
+    private static final String DATABASE_URL = "jdbc:mysql://ies9021.edu.ar:3306/ies9021_database";
+    private static final String DATABASE_USERNAME = "ies9021_userdb";
+    private static final String DATABASE_PASSWORD = "Xsw23edc.127";
+
+    public static Conection getConnection() throws SQLException{
+        try{
+            class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD)
+        }catch( ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void executeQuery(){
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try{
+            connection = getConnection();
+            String query = "SELECT * FROM branch";
+            statement = connection.prepareStatement(query);
+            resul = statement.executeQuery();
+
+            while(result.next()){
+                int id = result.getInt("id_branch");
+                String nombre = result.getString("name");
+
+                System.out.println("Branch: " + id + " " + nombre);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(result != null){
+                    result.close();
+                }
+                if(statement != null){
+                    statement.close();
+                }
+                if(connection != null){
+                    connection.close();
+                }
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
