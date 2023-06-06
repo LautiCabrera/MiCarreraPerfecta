0647651d3f1fd64891bf0f1227120fe0c35ae56f
IlkoArboit <ilkoarboi@gmail.com>
2023-06-06 12:07:22 -0300

    Actualización de DataFetcher y DBConnection +Solución de error causado por cerrar resultSet prematuramente +Cambios en la conexión a la bd

--- app/src/main/java/com/example/micarreraperfecta/Utils/JsonDataFetcher.java
+++ app/src/main/java/com/example/micarreraperfecta/Utils/JsonDataFetcher.java
@@ -1,52 +1,109 @@
+import java.sql.*;
+import java.util.List;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
-import java.sql.ResultSet;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import java.io.IOException;
 
 public class JsonDataFetcher<T> {
     private static ObjectMapper objectMapper = new ObjectMapper();
 
-    public static T fetchTableData(String tableName, String whereClause, Class<T> returnType){
+    /**
+     * Devuelve una lista de objetos de la clase espeficada.
+     * @param tableName El nombre de la tabla en la cual se encuentran los datos del objeto. Para este proyecto se encuentran como una constante dentro de la clase.
+     * @param whereClause Condición de busqueda en la tabla. La palabra "WHERE" está implicita, solo se debe poner el resto de la consulta.
+     * @param returnType Es la clase a la cual le vamos a asignar los datos traidos de la tabla. La lista se creará con objetos de esta clase dentro.
+     */
+    public static <T> List<T> fetchTableData(String tableName, String whereClause, Class<T> returnType) {
         String query = "SELECT * FROM " + tableName;
         if (whereClause != null && !whereClause.isEmpty()) {
             query += " WHERE " + whereClause;
         }
-
+        ResultSet resultSet = null;
+    
         try {
-            ResultSet resultSet = DBConection.executeQuery(query);
-
-            ObjectMapper mapper = new ObjectMapper();
-            ObjectNode rootNode = mapper.createObjectNode();
-            JsonNode resultNode = mapper.createArrayNode();
-
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int columnCount = metaData.getColumnCount();
-
-            while (resultSet.next()) {
-                ObjectNode rowNode = mapper.createObjectNode();
-
-                for (int i = 1; i <= columnCount; i++) {
-                    String columnName = metaData.getColumnName(i);
-                    Object columnValue = resultSet.getObject(i);
-                    rowNode.put(columnName, columnValue.toString());
+            resultSet = DBConection.executeQuery(query);
+    
+            if (resultSet != null) {
+                ObjectMapper mapper = new ObjectMapper();
+                ArrayNode resultNode = mapper.createArrayNode();
+    
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                int columnCount = metaData.getColumnCount();
+    
+                while (resultSet.next()) {
+                    ObjectNode rowNode = mapper.createObjectNode();
+    
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = metaData.getColumnName(i);
+                        Object columnValue = resultSet.getObject(i);
+                        rowNode.put(columnName, columnValue.toString());
+                    }
+    
+                    resultNode.add(rowNode);
                 }
-
-                ((ArrayNode) resultNode).add(rowNode);
+    
+                String jsonResult = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(resultNode);
+    
+                return mapper.readValue(jsonResult, mapper.getTypeFactory().constructCollectionType(List.class, returnType));
             }
-
-            rootNode.set("result", resultNode);
-            String jsonResult = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(rootNode);
-            return mapper.readValue(jsonResult, returnType);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
+    
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
+        } finally {
+            DBConection.closeResources(null, null, resultSet);
         }
-        
+        return null;
     }
+
+    /**
+     * Devuelve una lista de objetos de la clase espeficada.
+     * @param tableName El nombre de la tabla en la cual se encuentran los datos del objeto. Para este proyecto se encuentran como una constante dentro de la clase.
+     * @param returnType Clase a la cual se le van a asignar los datos traidos de la tabla. La lista se creará con objetos de esta clase dentro.
+     */
+    public static <T> List<T> fetchTableData(String tableName, Class<T> returnType) {
+        String query = "SELECT * FROM " + tableName;
+        
+        ResultSet resultSet = null;
+    
+        try {
+            resultSet = DBConection.executeQuery(query);
+    
+            if (resultSet != null) {
+                ObjectMapper mapper = new ObjectMapper();
+                ArrayNode resultNode = mapper.createArrayNode();
+    
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                int columnCount = metaData.getColumnCount();
+    
+                while (resultSet.next()) {
+                    ObjectNode rowNode = mapper.createObjectNode();
+    
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = metaData.getColumnName(i);
+                        Object columnValue = resultSet.getObject(i);
+                        rowNode.put(columnName, columnValue.toString());
+                    }
+    
+                    resultNode.add(rowNode);
+                }
+    
+                String jsonResult = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(resultNode);
+    
+                return mapper.readValue(jsonResult, mapper.getTypeFactory().constructCollectionType(List.class, returnType));
+            }
+    
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBConection.closeResources(null, null, resultSet);
+        }
+        return null;
+    }
+    
 
     // String json = "{\"atributo1\":\"valor1\",\"atributo2\":42}";
     public static <T> T createInstance(Class<T> type, String json) throws IOException {
