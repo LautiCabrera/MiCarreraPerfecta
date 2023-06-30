5f2ec8e0b342260002b4467bc73f2cc4789e4b7b
Ilko Esteban Arboit <ilkoarboit@gmail.com>
2023-06-30 20:15:56 -0300

    Actualización de componentes
    +Creación de sistema de logs para consultas a la bd
    +Creación de generador de clases en base a tablas

--- app/src/main/java/com/example/micarreraperfecta/Utils/DBConnection.java
+++ app/src/main/java/com/example/micarreraperfecta/Utils/DBConnection.java
@@ -1,3 +1,5 @@
+package com.example.micarreraperfecta.Utils;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,11 +16,13 @@ public class DBConection {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
-            System.out.println("Connection successful!");
+            
             return connection;
         } catch (ClassNotFoundException e) {
-            throw new SQLException("Failed to load database driver", e);
+            
+            e.printStackTrace();
         }
+        return null;
     }
 
     public static ResultSet executeQuery(String query) {
@@ -30,14 +34,33 @@ public class DBConection {
             connection = getConnection();
             statement = connection.prepareStatement(query);
             result = statement.executeQuery();
+            logConnection("Conexión exitosa", query);
 
             return result;
         } catch (SQLException e) {
+            logConnection("Conexión fallida", query);
             e.printStackTrace();
             closeResources(connection, statement, result);
         }
-    
+
         return null;
+    }
+
+    public static void logConnection(String title, String description) {
+        String insertQuery = "INSERT INTO logs (title, description, id_user, date) VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+                PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+
+            preparedStatement.setString(1, title);
+            preparedStatement.setString(2, description);
+            preparedStatement.setInt(3, 6);
+            preparedStatement.setString(4, LocalDate.now().toString());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void closeResources(Connection connection, PreparedStatement statement, ResultSet result) {
