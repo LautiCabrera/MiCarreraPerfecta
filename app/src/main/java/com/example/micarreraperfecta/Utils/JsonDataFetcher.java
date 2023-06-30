04c8d8761161387348959c911ff355542a013664
Ilko Esteban Arboit <ilkoarboit@gmail.com>
2023-06-30 20:15:56 -0300

    Actualización de componentes
    +Creación de sistema de logs para consultas a la bd
    +Creación de generador de clases en base a tablas

--- app/src/main/java/com/example/micarreraperfecta/Utils/JsonDataFetcher.java
+++ app/src/main/java/com/example/micarreraperfecta/Utils/JsonDataFetcher.java
@@ -1,21 +1,14 @@
+package com.example.micarreraperfecta.Utils;
 import java.sql.*;
-import java.util.List;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-
-import java.io.IOException;
 
 public class JsonDataFetcher<T> {
-    private static ObjectMapper objectMapper = new ObjectMapper();
-
     /**
-     * Devuelve una instancia de la clase especificada. La instancia devuelta es la primera de la tabla correspondiente a la clase que cumple con la concición de la clausula WHERE.
+     * Devuelve una instancia de la clase especificada. La instancia devuelta es la primera de la tabla correspondiente a la clase que cumple con la concici�n de la clausula WHERE.
      * @param tableName El nombre de la tabla en la cual se encuentran los datos del objeto. Para este proyecto se encuentran como una constante dentro de la clase.
-     * @param whereClause Condición de busqueda en la tabla. La palabra "WHERE" está implicita, solo se debe poner el resto de la consulta.
-     * @param returnType Es la clase a la cual le vamos a asignar los datos traidos de la tabla. La lista se creará con objetos de esta clase dentro.
+     * @param whereClause Condici�n de busqueda en la tabla. La palabra "WHERE" est� implicita, solo se debe poner el resto de la consulta.
+     * @param returnType Es la clase a la cual le vamos a asignar los datos traidos de la tabla. La lista se crear� con objetos de esta clase dentro.
      */
     public static <T> T fetchTableData(String tableName, String whereClause, Class<T> returnType) {
     String query = "SELECT * FROM " + tableName;
@@ -25,7 +18,7 @@ public class JsonDataFetcher<T> {
     ResultSet resultSet = null;
 
     try {
-        resultSet = DBConection.executeQuery(query);
+        resultSet = DBConnection.executeQuery(query);
 
         if (resultSet != null && resultSet.next()) {
             ObjectMapper mapper = new ObjectMapper();
@@ -47,7 +40,7 @@ public class JsonDataFetcher<T> {
     } catch (Exception e) {
         e.printStackTrace();
     } finally {
-        DBConection.closeResources(null, null, resultSet);
+        DBConnection.closeResources(null, null, resultSet);
     }
 
     return null;
@@ -57,7 +50,7 @@ public class JsonDataFetcher<T> {
     /**
      * Devuelve una instancia de la clase especificada. La instancia devuelta es la primera de la tabla correspondiente a la clase.
      * @param tableName El nombre de la tabla en la cual se encuentran los datos del objeto. Para este proyecto se encuentran como una constante dentro de la clase.
-     * @param returnType Clase a la cual se le van a asignar los datos traidos de la tabla. La lista se creará con objetos de esta clase dentro.
+     * @param returnType Clase a la cual se le van a asignar los datos traidos de la tabla. La lista se crear� con objetos de esta clase dentro.
      */
     public static <T> T fetchTableData(String tableName, Class<T> returnType) {
     String query = "SELECT * FROM " + tableName;
@@ -65,7 +58,7 @@ public class JsonDataFetcher<T> {
     ResultSet resultSet = null;
 
     try {
-        resultSet = DBConection.executeQuery(query);
+        resultSet = DBConnection.executeQuery(query);
 
         if (resultSet != null && resultSet.next()) {
             ObjectMapper mapper = new ObjectMapper();
@@ -87,7 +80,7 @@ public class JsonDataFetcher<T> {
     } catch (Exception e) {
         e.printStackTrace();
     } finally {
-        DBConection.closeResources(null, null, resultSet);
+        DBConnection.closeResources(null, null, resultSet);
     }
 
     return null;
@@ -96,7 +89,7 @@ public class JsonDataFetcher<T> {
     /**
      * Devuelve una instancia de la clase especificada si no hay una instancia existente. La instancia devuelta es la primera de la tabla correspondiente a la clase.
      * @param tableName El nombre de la tabla en la cual se encuentran los datos del objeto. Para este proyecto se encuentran como una constante dentro de la clase.
-     * @param returnType Clase a la cual se le van a asignar los datos traidos de la tabla. La lista se creará con objetos de esta clase dentro.
+     * @param returnType Clase a la cual se le van a asignar los datos traidos de la tabla. La lista se crear� con objetos de esta clase dentro.
      */
     public static <T> T fetchSingletonTableData(String tableName, String whereClause, Class<T> returnType) {
         String query = "SELECT * FROM " + tableName;
@@ -107,7 +100,7 @@ public class JsonDataFetcher<T> {
         ResultSet resultSet = null;
 
         try {
-            resultSet = DBConection.executeQuery(query);
+            resultSet = DBConnection.executeQuery(query);
 
             if (resultSet != null && resultSet.next()) {
                 ObjectMapper mapper = new ObjectMapper();
@@ -123,13 +116,13 @@ public class JsonDataFetcher<T> {
 
                 String jsonResult = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(rowNode);
 
-                return mapper.readValue(jsonResult, returnType.getMethod("getInstance").getReturnType());
+                return (T) mapper.readValue(jsonResult, returnType.getMethod("getInstance").getReturnType());
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            DBConection.closeResources(null, null, resultSet);
+            DBConnection.closeResources(null, null, resultSet);
         }
 
         return null;
